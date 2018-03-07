8f4fe7972dd012e390bbe938b1b3f1937ad2631e
Jose Luis Narváez <celunarvaez@gmail.com>
2018-03-07 08:48:53 +0100

    Se permite al usuario recordar su sesión durante 30 días

--- db/tradegame.sql
+++ db/tradegame.sql
@@ -11,9 +11,10 @@ CREATE TABLE usuarios
   , email        varchar(100) NOT NULL
   , password     varchar(255) NOT NULL
   , token_val    varchar(255)
+  , auth_key     varchar(255)
   , nombre_real  varchar(255)
   , localidad    varchar(255)
 );
 
-INSERT INTO usuarios (usuario, email, password)
-    VALUES ('admin', 'admin@admin.com', crypt('admin123', gen_salt('bf', 13)));
+INSERT INTO usuarios (usuario, email, password, auth_key)
+    VALUES ('admin', 'admin@admin.com', crypt('admin123', gen_salt('bf', 13)), 'GnT4M2ZjLDGxNrGe-2THbAjqFLwyJ1fa');
