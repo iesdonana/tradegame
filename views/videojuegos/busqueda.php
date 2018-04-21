fe3afa956feb5ad5482a8372ca6a709bc1e4570e
Jose Luis Narváez <celunarvaez@gmail.com>
2018-04-21 14:50:15 +0200

    Funcionalidad en filtros con JS

--- views/videojuegos/busqueda.php
+++ views/videojuegos/busqueda.php
@@ -1,10 +1,11 @@
 <?php
 
+use app\helpers\Utiles;
+
 use yii\helpers\Url;
 
 $url = Url::to(['videojuegos/vista-busqueda']);
 $js = <<<JS
-
 $('input[type=checkbox]').on('click', function() {
     var plataformas = [];
     var generos = [];
@@ -33,48 +34,67 @@ $('input[type=checkbox]').on('click', function() {
         }
     });
 });
+
+$('.filtros h4').on('click', function() {
+    var filtrosElems = $(this).closest('.row').next();
+    if (filtrosElems.css('display') == 'block') {
+        $(this).find('svg').remove();
+        $(this).append('<i class="fa fa-angle-down"></i>')
+        filtrosElems.slideUp();
+    } else {
+        $(this).find('svg').remove();
+        $(this).append('<i class="fa fa-angle-up"></i>')
+        filtrosElems.slideDown();
+    }
+});
 JS;
 $this->registerJs($js);
 $this->registerCssFile('@web/css/checkbox.css');
 ?>
 
-<div class="col-md-2">
+<div class="col-md-2 filtros">
     <div class="row">
         <div class="col-md-12">
-            <h4>Plataformas</h4>
+            <h4>Plataformas <?= Utiles::FA('angle-up') ?></h4>
         </div>
     </div>
-    <?php foreach ($plataformas as $key => $plat): ?>
-        <div class="col-md-12">
-            <?php $id = 'plat' . $plat->id ?>
-            <input id="<?= $id ?>" type="checkbox" name="plataforma" value="<?= $plat->id ?>">
-            <label for="<?= $id ?>"><?= $plat->nombre ?></label>
-        </div>
-    <?php endforeach; ?>
+    <div class="row filtros-datos">
+        <?php foreach ($plataformas as $key => $plat): ?>
+            <div class="col-md-12">
+                <?php $id = 'plat' . $plat->id ?>
+                <input id="<?= $id ?>" type="checkbox" name="plataforma" value="<?= $plat->id ?>">
+                <label for="<?= $id ?>"><?= $plat->nombre ?></label>
+            </div>
+        <?php endforeach; ?>
+    </div>
     <div class="row">
         <div class="col-md-12">
-            <h4>Géneros</h4>
+            <h4>Géneros <?= Utiles::FA('angle-up') ?></h4>
         </div>
     </div>
-    <?php foreach ($generos as $gen): ?>
-        <div class="col-md-12">
-            <?php $id = 'gen' . $gen->id ?>
-            <input id="<?= $id ?>" type="checkbox" name="generos_videojuegos" value="<?= $gen->id ?>">
-            <label for="<?= $id ?>"><?= $gen->nombre ?></label>
-        </div>
-    <?php endforeach; ?>
+    <div class="row filtros-datos">
+        <?php foreach ($generos as $gen): ?>
+            <div class="col-md-12">
+                <?php $id = 'gen' . $gen->id ?>
+                <input id="<?= $id ?>" type="checkbox" name="generos_videojuegos" value="<?= $gen->id ?>">
+                <label for="<?= $id ?>"><?= $gen->nombre ?></label>
+            </div>
+        <?php endforeach; ?>
+    </div>
     <div class="row">
         <div class="col-md-12">
-            <h4>Desarrolladores</h4>
+            <h4>Desarrolladores <?= Utiles::FA('angle-up') ?></h4>
         </div>
     </div>
-    <?php foreach ($desarrolladores as $des): ?>
-        <div class="col-md-12">
-            <?php $id = 'des' . $des->id ?>
-            <input id="<?= $id ?>" type="checkbox" name="desarrolladores" value="<?= $des->id ?>">
-            <label for="<?= $id ?>"><?= $des->compania ?></label>
-        </div>
-    <?php endforeach; ?>
+    <div class="row filtros-datos">
+        <?php foreach ($desarrolladores as $des): ?>
+            <div class="col-md-12">
+                <?php $id = 'des' . $des->id ?>
+                <input id="<?= $id ?>" type="checkbox" name="desarrolladores" value="<?= $des->id ?>">
+                <label for="<?= $id ?>"><?= $des->compania ?></label>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </div>
 <div class="col-md-10">
     <div class="panel panel-default">
