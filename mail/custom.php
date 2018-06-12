d2a1a478837869c0e004b297bd70fcaaa4628f1f
Jose Luis Narváez <celunarvaez@gmail.com>
2018-06-12 10:52:40 +0200

    Fix diseño email para mobile

--- mail/custom.php
+++ mail/custom.php
@@ -1,7 +1,6 @@
 <?php
 
 use yii\helpers\Html;
-use yii\helpers\Url;
 
 /* @var $this \yii\web\View view component instance */
 /* @var $message \yii\mail\MessageInterface the message being composed */
@@ -15,71 +14,78 @@ use yii\helpers\Url;
     <?php $this->head() ?>
 <?php
     $css = <<<'CSS'
-        .titulo {
-            background-color: grey;
-        }
+    .titulo {
+        background-color: grey;
+    }
 
-        .row {
-            margin-right: 200px;
-            margin-left: 200px;
-        }
+    body {
+        background-color: #cecece;
+        font-family: 'Raleway'
+    }
 
-        .row .texto {
-            border: 2px dashed grey;
-        }
+    .row {
+        margin-right: 200px;
+        margin-left: 200px;
+    }
 
-        .texto {
-            padding: 20px;
-        }
+    .texto {
+        background-color: #efefef;
+        padding: 10px;
+    }
 
-        img {
-            display: block;
-            margin: auto;
-        }
+    img {
+        display: block;
+        margin: auto;
+    }
 
-        .url {
-            font-weight: bold;
-        }
+    .url {
+        font-weight: bold;
+    }
 
-        a.boton:hover {
-            color: white;
-            text-decoration: none;
-            background-color: #55a34b;
-        }
+    a.boton:hover {
+        color: white;
+        text-decoration: none;
+        background-color: #55a34b;
+    }
 
-        .boton, .oferta {
-            text-decoration: none;
-            cursor: pointer;
-            display: block;
-            margin-top: 20px;
-            color: white;
-            padding: 10px;
-            text-align:center;
-        }
+    .boton, .oferta {
+        text-decoration: none;
+        cursor: pointer;
+        display: block;
+        margin-top: 20px;
+        color: white;
+        padding: 10px;
+        text-align:center;
+    }
 
-        .oferta {
-            background-color: #730000;
-        }
+    .oferta {
+        background-color: #730000;
+    }
 
-        .boton {
-            background-color: #248217;
-        }
+    .boton {
+        background-color: #248217;
+    }
 CSS;
 
     $this->registerCss($css);
     ?>
 </head>
 <body>
-    <div class="row titulo">
-        <?= Html::img('https://raw.githubusercontent.com/jlnarvaez/tradegame/master/web/titulo.png', [
-            'height' => '80px',
-        ]) ?>
-    </div>
-    <div class="row">
-        <div class="texto">
-            ¡Hola, <?= $usuario ?>! <br>
-            <?= $content ?>
-        </div>
-    </div>
+    <table align="center" width="600">
+        <tr>
+            <td class="titulo">
+                <?= Html::img('https://raw.githubusercontent.com/jlnarvaez/tradegame/master/web/titulo.png', [
+                    'height' => '80',
+                ]) ?>
+            </td>
+        </tr>
+        <tr>
+            <td class="texto">
+                ¡Hola, <strong><?= $usuario ?></strong>! <br>
+                <?= $content ?>
+
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
